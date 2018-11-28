4c18c94c530ecf696db1bdc0ecd11803ce7567d8
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-28 14:31:05 +1100

    Changed MockProduct to JSON Server Data

--- src/libs/product/api.js
+++ src/libs/product/api.js
@@ -1,6 +1,5 @@
-import mockProducts from './products.mock';
-
-export const fetchAll = async()=> mockProducts;
+const url = 'http://localhost:3001/data'
+export const fetchAll = async()=> fetch(url).then(res=>res.json());
 
 // export const fetchAll = async()=>{
 //   throw('Product is not found')
